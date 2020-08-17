d6b12562411522bcaa669dd57cadc62ecc00ab00
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-08-17 15:43:37 -0300

    Feat: add h2 and configurations

--- EpicTask/src/main/java/br/com/fiap/EpicTask/controller/UserController.java
+++ EpicTask/src/main/java/br/com/fiap/EpicTask/controller/UserController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.com.fiap.EpicTask.model.User;
 import br.com.fiap.EpicTask.repository.UserRepository;
@@ -24,7 +23,7 @@ public class UserController {
 	public String save(User user) {
 		System.out.println(user);
 		repository.save(user);
-		return "users";
+		return "redirect:user";
 	}
 	
 	@RequestMapping("/user/new")
--- EpicTask/src/main/java/br/com/fiap/EpicTask/model/User.java
+++ EpicTask/src/main/java/br/com/fiap/EpicTask/model/User.java
@@ -5,11 +5,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity
+@Entity(name = "users")
 public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
 	private String name;
 	
 	
@@ -19,18 +21,27 @@ public class User {
 	private String pass;
 	
 	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getEmail() {
 		return email;
 	}
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
 	public String getPass() {
 		return pass;
 	}
