63f5e4c83ad245c4885b30ecc8bfe51a9857f2fe
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-08-17 08:48:35 -0300

    Feat: start project with user controller

---
+++ EpicTask/src/main/java/br/com/fiap/EpicTask/controller/UserController.java
@@ -0,0 +1,15 @@
+package br.com.fiap.EpicTask.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserController {
+
+	@RequestMapping("/users")
+	@ResponseBody
+	public String users() {
+		return "Equipe";
+	}
+}
