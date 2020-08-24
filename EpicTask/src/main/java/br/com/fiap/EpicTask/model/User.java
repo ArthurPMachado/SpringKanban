1770cf82ae90308eb902ed4d13d28a9acf8ca178
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-08-24 11:10:51 -0300

    Feat: add bean validation

--- EpicTask/src/main/java/br/com/fiap/EpicTask/model/User.java
+++ EpicTask/src/main/java/br/com/fiap/EpicTask/model/User.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 @Entity(name = "users")
 public class User {
@@ -12,6 +13,7 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotBlank
 	private String name;
 	
 	
