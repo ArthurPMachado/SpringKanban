586397fd6c315372ff75dbc29b7601fb9851967f
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-09-17 23:16:38 -0300

    Feat: create task model

--- EpicTask/src/main/java/br/com/fiap/EpicTask/controller/TaskController.java
+++ EpicTask/src/main/java/br/com/fiap/EpicTask/controller/TaskController.java
@@ -1,13 +1,21 @@
 package br.com.fiap.EpicTask.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/task")
 public class TaskController {
+	/*
+	@Autowired
+	private TaskRepository repository;
 
-	@RequestMapping("/task")
-	public String tasks() {
-		return "tasks";
+	@GetMapping
+	public ModelAndView tasks() {
+		
 	}
+	*/
 }
