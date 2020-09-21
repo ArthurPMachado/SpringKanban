47abb52467ddad2d6ba5359d2ae5a6790cc1a1e4
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-09-21 11:33:15 -0300

    Feat: create authentication service

--- EpicTask/src/main/java/br/com/fiap/EpicTask/security/SecurityConfiguration.java
+++ EpicTask/src/main/java/br/com/fiap/EpicTask/security/SecurityConfiguration.java
@@ -1,6 +1,8 @@
 package br.com.fiap.EpicTask.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,13 +11,24 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private AuthenticationService authenticationService;
+
+	// autenticacao
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(authenticationService);
+	}
+	
 	// autorizacao
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers("/user/**")
+			.antMatchers("/task/**", "/user/**")
 				.authenticated()
 				.and()
+		//	.csrf()
+		//		.disable()
 			.formLogin();
 	}
 	
