ea1d0ae896971b68586da91aa2de8f9aef887f74
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-09-28 12:08:40 -0300

    Feat: add take and drop tasks

--- EpicTask/src/main/java/br/com/fiap/EpicTask/controller/TaskController.java
+++ EpicTask/src/main/java/br/com/fiap/EpicTask/controller/TaskController.java
@@ -1,11 +1,13 @@
 package br.com.fiap.EpicTask.controller;
 
+import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,7 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.fiap.EpicTask.model.Task;
+import br.com.fiap.EpicTask.model.User;
 import br.com.fiap.EpicTask.repository.TaskRepository;
 
 @Controller
@@ -70,6 +73,41 @@ public class TaskController {
 		repository.deleteById(id);
 		attribute.addFlashAttribute("msg", "Task removida com sucesso");
 		return "redirect:/task";
+	}
+	
+	@GetMapping("/take/{id}")
+	public String take(@PathVariable Long id, Authentication auth) {
+		//carregar a tarefa
+		Optional<Task> task = repository.findById(id);
+		if (task.isPresent()) {
+			Task newTask = task.get();
+			
+			if (newTask.getUser() == null) {
+				//carregar user
+				User user = (User) auth.getPrincipal();
+				
+				//atribuir a tarefa
+				newTask.setUser(user);
+				repository.save(newTask);
+			}
+		}
+		return "redirect:/task"; 
+	}
+	
+	@GetMapping("/drop/{id}")
+	public String drop(@PathVariable Long id, Principal auth) {
+		//carregar a tarefa
+		Optional<Task> task = repository.findById(id);
+		if (task.isPresent()) {
+			Task newTask = task.get();
+			
+			if (newTask.getUser().getEmail().equals(auth.getName())) {
+				newTask.setUser(null);
+				repository.save(newTask);
+			}
+			
+		}
+		return "redirect:/task"; 
 	}
 	
 }
