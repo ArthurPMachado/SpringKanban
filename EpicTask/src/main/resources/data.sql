b9014ca2dafc53b4a1b33d0f2de19760bb80b18f
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-09-28 12:00:20 -0300

    Feat: add default tasks

--- EpicTask/src/main/resources/data.sql
+++ EpicTask/src/main/resources/data.sql
@@ -1,1 +1,8 @@
 INSERT INTO users (name, email, pass) VALUES ('Arthur pereira machado', 'arthurpereiramachado01@gmail.com', '$2a$10$sZjYjnOGPJsj9WSCXEIR5OP20.aJTNolnZaUCBHd0qS2A5u3d7Idm');
+INSERT INTO users (name, email, pass) VALUES ('Felipe Correa Tamizaki', 'felipe@gmail.com', '$2a$10$cVAOGC17dtg/nb5ChZVNdu7SKEWcWNoLT5WgqH9w8/gKin19knzOG');
+
+INSERT INTO tasks (title, description, point, status, user_id) VALUES
+('Analise', 'analise completa do sistemas', 100, 90, null),
+('Protype', 'prototipacao das telas', 100, 70, null),
+('Data base', 'criacao do banco de dados', 50, 10, 1),
+('Teste', 'teste de integração', 80, 20, 2);
