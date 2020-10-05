6001bbde28f5a4f754eada9fab4f74fb28d3ae27
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-10-05 11:02:03 -0300

    Feat: add role to users

--- EpicTask/src/main/resources/data.sql
+++ EpicTask/src/main/resources/data.sql
@@ -1,5 +1,9 @@
-INSERT INTO users (name, email, pass) VALUES ('Arthur pereira machado', 'arthurpereiramachado01@gmail.com', '$2a$10$sZjYjnOGPJsj9WSCXEIR5OP20.aJTNolnZaUCBHd0qS2A5u3d7Idm');
-INSERT INTO users (name, email, pass) VALUES ('Felipe Correa Tamizaki', 'felipe@gmail.com', '$2a$10$cVAOGC17dtg/nb5ChZVNdu7SKEWcWNoLT5WgqH9w8/gKin19knzOG');
+INSERT INTO users (name, email, pass, git_hub_user) VALUES ('Arthur pereira machado', 'arthurpereiramachado01@gmail.com', '$2a$10$sZjYjnOGPJsj9WSCXEIR5OP20.aJTNolnZaUCBHd0qS2A5u3d7Idm', 'ArthurPMachado');
+INSERT INTO users (name, email, pass, git_hub_user) VALUES ('Felipe Correa Tamizaki', 'felipe@gmail.com', '$2a$10$cVAOGC17dtg/nb5ChZVNdu7SKEWcWNoLT5WgqH9w8/gKin19knzOG', 'felipeTamizaki');
+
+INSERT INTO role (name) VALUES ('ROLE_ADMIN'), ('ROLE_USER');
+
+INSERT INTO users_roles VALUES (1, 1), (2, 2);
 
 INSERT INTO tasks (title, description, point, status, user_id) VALUES
 ('Analise', 'analise completa do sistemas', 100, 90, null),
